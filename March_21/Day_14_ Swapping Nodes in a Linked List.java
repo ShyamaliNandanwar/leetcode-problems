ad5e2ee387182314fea141fe45ff25044b4c17ca
Shyamali Nandanwar <shyamali341@gmail.com>
2021-03-15 00:33:44 +0530

    2

---
+++ March_21/Day_14_ Swapping Nodes in a Linked List.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ArrayList<Integer> al = new ArrayList<>();
+        ListNode node = head;
+        while(node!=null){
+            al.add(node.val);
+            node = node.next;
+        }
+        if(k > al.size())
+            return head;
+        Integer a[] = new Integer[al.size()];
+        a = al.toArray(a);
+        k =k-1;
+        
+        int l = 0;
+        if(k+1 != a.length)
+            l = a.length-k-1;
+        //System.out.println(k + " " + l);
+        int temp = a[k];
+        a[k]=a[l];
+        a[l]=temp;
+        node = head;
+        for(int i=0;i< a.length;i++){
+            if(node.val !=a[i])
+                node.val=a[i];
+            node = node.next;
+        }
+        return head;
+            
+        
+    }
+}

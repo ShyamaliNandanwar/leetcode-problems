37e98b21f70ee2f8cb9b70f3d39cd8884a3da8e8
Shyamali Nandanwar <shyamali341@gmail.com>
2021-04-21 15:33:20 +0530

    day20

---
+++ April_21/Day_20_N-ary Tree Preorder Traversal  Solution.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+
+class Solution {
+    List<Integer> res = null ;
+    public List<Integer> preorder(Node root) {
+        res = new ArrayList<>();
+        if(root == null)
+            return res;
+        
+        if(root.children == null || root.children.size() == 0 ){
+            res.add(root.val);
+             return res;
+        }
+           
+       Stack<Node> q = new Stack<>();
+
+        Node node = root;
+        q.add(root);
+        while(!q.isEmpty()){
+            Node n= q.pop();
+            res.add(n.val);
+            int i=n.children.size()-1;
+            while(i >-1 ){
+                q.push(n.children.get(i));
+                i--;
+            }
+        }
+        return res;
+    }
+}

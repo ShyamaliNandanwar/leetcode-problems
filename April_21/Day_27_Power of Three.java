796f70cd77c0e8e03d4a85d20bdf5ae193543f8b
Shyamali Nandanwar <shyamali341@gmail.com>
2021-04-27 23:44:26 +0530

    day27

---
+++ April_21/Day_27_Power of Three.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==0)
+            return false;
+        double t = Math.log10(n)/Math.log10(3);
+        //System.out.println(t);
+        if(t%1==0)
+            return true;
+        return false;
+        
+    }
+}

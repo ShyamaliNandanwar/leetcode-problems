23c7d9ab0172528f22470b4b606dc979a0e4ae98
Shyamali Nandanwar <shyamali341@gmail.com>
2021-05-26 00:30:29 +0530

    day25

---
+++ May_21/Day_25_ Evaluate Reverse Polish Notation.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        int res=0,n=0;
+        Stack<Integer> st = new Stack<>();
+        if(tokens.length ==1)
+            return Integer.parseInt(tokens[0]);
+        for(String s: tokens){
+            if(s.equals("+") || s.equals("-") || s.equals("*")||s.equals("/")){
+                res=st.pop();
+                n=st.pop();
+
+                switch(s){
+                    case "+": res = res+n;
+                        break;
+                    case "-": res = n-res;
+                        break;
+                    case "*": res = res*n;
+                        break;
+                    case "/": res = n/res;
+                        break;
+                }
+                st.push(res);
+            }else{
+                st.push(Integer.parseInt(s));
+            }
+            
+        }
+        return res;
+    }
+}

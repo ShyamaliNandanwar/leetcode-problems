d333dfb9f36559c6deddd60eb069ddfb0c9a4b2c
Shyamali Nandanwar <shyamali341@gmail.com>
2022-03-12 00:17:58 +0530

    lc

---
+++ March_22/Add Two Numbers.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry =0;
+        if(l1==null)
+            return l2;
+        if(l2==null)
+            return l1;
+        ListNode head = null;
+        ListNode node = head;
+        int sum =0 ;
+        while(l1!=null && l2!=null){
+            sum= l1.val+l2.val+carry;
+            carry=sum/10;
+            if(sum > 9){
+                sum = sum%10;
+            }
+
+            l1 = l1.next;
+            l2 = l2.next;
+            if(node == null){
+                node = new ListNode(sum);
+                head = node;
+            }else{
+                node.next = new ListNode(sum);
+                node = node.next;
+            }
+        }
+
+        while(l1!=null){
+            sum = l1.val+carry;
+            carry = sum/10;
+            if(sum>9)
+                sum = sum%10;
+
+            l1=l1.next;
+            node.next = new ListNode(sum);
+            node = node.next;
+        }
+
+        while(l2!=null){
+            sum = l2.val+carry;
+            carry = sum/10;
+            if(sum>9)
+                sum = sum%10;
+            l2=l2.next;
+            node.next = new ListNode(sum);
+            node = node.next;
+        }
+
+        if(carry >0){
+            node.next = new ListNode(carry);
+            node=node.next;
+        }
+
+        return head;
+    }
+}
---
+++ March_22/Rotate List.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null || k==0)
+            return head;
+        int length = 0;
+        ListNode node = head;
+        while(node!=null){
+            length++;
+            node=node.next;
+        }
+
+        if(k>=length)
+            k = k%length;
+
+        if(k==0)
+            return head;
+        else
+            k = length - k;
+
+        node = head;
+        ListNode prev = head;
+        while(k > 0){
+            prev = node;
+            node=node.next;
+            k--;
+        }
+        ListNode newHead = node;
+        prev.next = null;
+
+        node = newHead;
+        while(node.next!=null)
+            node= node.next;
+        node.next = head;
+        return newHead;
+    }
+}

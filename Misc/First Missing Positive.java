d700fbb610ab25b669ed6d317d8338b7ac0cd284
Shyamali Nandanwar <shyamali341@gmail.com>
2022-06-23 12:58:31 +0530

    lc

---
+++ Misc/First Missing Positive.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        for(int i=0; i< nums.length; i++)
+            ts.add(nums[i]);
+
+        int m = ts.last();
+        for(int i=1; i< m; i++){
+            if(!ts.contains(i))
+                return i;
+        }
+        return m > 0 ? m+1 : 1;
+    }
+}
---
+++ MonthlyChallenges/May_22/Backspace String Compare.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        String s1 = calculate(s);
+        String t1 = calculate(t);
+
+        return s1.equals(t1);
+    }
+
+    private String calculate(String s){
+        Stack<Character> st = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(!st.empty() && c == '#')
+                st.pop();
+            else if(c != '#')
+                st.push(c);
+        }
+
+        s = new String();
+        while(!st.empty())
+            s = s+ st.pop();
+
+        return s;
+    }
+}

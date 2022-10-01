6a1001c98efa21099e4e7a057cf4428c5c8ba4df
shyamalinandanwar <shyamali341@gmail.com>
2022-10-02 01:50:01 +0530

    lc

---
+++ Misc/Merge Intervals.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length == 0)
+            return intervals;
+
+        List<int[]> li = new ArrayList<>();
+        Arrays.sort(intervals, (a,b)-> a[0] -b[0]);
+
+        int n[] = intervals[0];
+        int i =1;
+
+        while(i < intervals.length){
+            if(n[1] >= intervals[i][0])
+                n[1] = Math.max(intervals[i][1], n[1]);
+            else{
+                li.add(n);
+                n = intervals[i];
+            }
+            i++;
+        }
+        li.add(n);
+
+        return li.toArray(new int[li.size()][]);
+    }
+}
